768e6dc3b3ab280905301a1cdf40b12efd0bfe25
zhaozuyuan <zhaozuyuan@kuaishou.com>
2021-02-04 14:04:07 +0800

    收集信息

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-### 基于WiFi和PDR的室内定位系统设计
+## 基于WiFi和PDR的室内定位系统设计
+
+### 关键传感器
+- TYPE_ACCELEROMETER：加速度传感器，获取x y z 上的加速度
+
+https://stackoom.com/question/3ZwAT/%E9%9D%99%E6%AD%A2%E6%97%B6%E7%94%A8TYPE-ROTATION-VECTOR%E5%AE%9E%E7%8E%B0%E5%BD%93%E5%89%8D%E7%9A%84%E5%9F%BA%E6%9C%AC%E6%96%B9%E5%90%91%E6%B3%95
+https://stackoverflow.com/questions/37531143/difference-between-sensor-type-rotation-vector-and-getorientation-by-combining-t/42772927#42772927
+https://www.jianshu.com/p/f2cce22280df
--- lib_pdr/src/main/java/com/zzy/lib_pdr/PDRActivity.kt
+++ lib_pdr/src/main/java/com/zzy/lib_pdr/PDRActivity.kt
@@ -1,13 +1,23 @@
 package com.zzy.lib_pdr
 
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_pdr.*
 
 
 class PDRActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pdr)
         supportActionBar?.hide()
+
+        btnStart.setOnClickListener {
+            val sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+            //获取线性加速度传感器
+            sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
+        }
     }
 }
